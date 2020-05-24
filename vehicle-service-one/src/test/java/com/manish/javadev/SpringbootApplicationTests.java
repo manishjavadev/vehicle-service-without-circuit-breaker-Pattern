f1078292bbafa9fdffcebf49ddd0133260a1c0a0
kmamani <kmamani@KMAMANI-IN.in.oracle.com>
2020-05-24 15:41:34 +0530

    Test CLass Renamed

--- vehicle-service-one/src/test/java/com/manish/javadev/SpringbootApplicationTests.java
+++ vehicle-service-one/src/test/java/com/manish/javadev/SpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.manish.javadev.springbootserviceone;
+package com.manish.javadev;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootServiceOneApplicationTests {
+public class SpringbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
